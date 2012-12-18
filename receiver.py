5122fe334163e05dfbfbe9f505b48cfd718e3cd7
Sergio Tanzilli <tanzilli@sergio-eeepc.(none)>
2012-12-18 01:54:04 +0100

    Use thebasic TouchOSC panel

--- receiver.py
+++ receiver.py
@@ -8,36 +8,23 @@ import types
 
 server = OSCServer( ("0.0.0.0", 8000) )
 client = OSCClient()
-client.connect( ("192.168.1.3", 9000) )
+client.connect( ("192.168.1.2", 9000) )
 
 def handle_timeout(self):
 	print ("Timeout")
 
 server.handle_timeout = types.MethodType(handle_timeout, server)
 
-def user_callback(path, tags, args, source):
-#	global client
+def fader_callback(path, tags, args, source):
 	print ("path", path) 
 	print ("args", args) 
 	print ("source", source) 
-	msg=OSCMessage("/2/fader2")
-	msg.append(args);
-	client.send(msg)
-	msg=OSCMessage("/2/fader3")
-	msg.append(args);
-	client.send(msg)
-	msg=OSCMessage("/2/rotary1")
+	msg=OSCMessage("/1/rotary1")
 	msg.append(args);
 	client.send(msg)
 
-def toggle33_callback(path, tags, args, source):
-	print ("path", path) 
-	print ("args", args) 
-	print ("source", source) 
 
-
-server.addMsgHandler( "/2/fader1",user_callback)
-server.addMsgHandler( "/6/toggle33",toggle33_callback)
+server.addMsgHandler( "/1/fader1",fader_callback)
 
 while True:
 	server.handle_request()
